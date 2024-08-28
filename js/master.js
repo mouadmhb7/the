d02f47b0413ea0532122e39e26b462b56b1ba419
mouad <mouadmhb6@gmail.com>
2024-08-28 12:52:46 +0200

    Update master.js

--- js/master.js
+++ js/master.js
@@ -1,4 +1,20 @@
+// select landing page element 
+let landingPge = document.querySelector(".landing-page");
+
+// Get array of images
+let imgsArray = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg"];
+
+
+
+
+// Get a Random Number 
 
 
 
 setInterval(() => {
+
+    let randomNumber = Math.floor(Math.random() * imgsArray.length);
+   
+// Change Background Image Url 
+    landingPge.style.backgroundImage = "url('imgs/" + imgsArray[randomNumber] + "')"
+}, 10000);
